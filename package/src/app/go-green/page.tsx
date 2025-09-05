1a1346f0a4b3d38779c9ac2701ac994becf8d788
thanakon-film60 <thanakon.film60@gmail.com>
2025-09-05 10:18:39 +0700

    update TPP -3

--- package/src/app/go-green/page.tsx
+++ package/src/app/go-green/page.tsx
@@ -3,7 +3,7 @@ import GoGreenSection from "./GoGreenSection";
 export default function Page() {
   return (
     <GoGreenSection
-      title="Sustainability at TPP"
+      title={`" ขับเคลื่อนการเติบโตอย่างยั่งยืน เพื่อธุรกิจ สังคม และโลกใบนี้ "`}
       imageFit="cover"
       textFirstOnMobile={false}
       imagePosition="left"
